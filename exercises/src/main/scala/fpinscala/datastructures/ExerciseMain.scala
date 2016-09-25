ba5ff338f9fdd0ed4e8c9187656d73a17b38e7f2
Sho Nakatani <lay.sakura@gmail.com>
2016-09-25 15:26:40 +0900

    exercise 3.2

--- exercises/src/main/scala/fpinscala/datastructures/ExerciseMain.scala
+++ exercises/src/main/scala/fpinscala/datastructures/ExerciseMain.scala
@@ -8,5 +8,18 @@ object ExerciseMain {
          |  That complex pattern match should yeild ... `3`, which is from `case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y`
          |  Answer: ${List.x}
        """.stripMargin)
+
+    { // exercise 3.2
+      val l1 = List(1, 2, 3, 4)
+      val l2 = List(1)
+      val l3 = List()
+      println(
+        s"""
+           |exercise 3.2
+           |  Remove head from $l1 => ${List.tail(l1)}
+           |  Remove head from $l2 => ${List.tail(l2)}
+           |  Remove head from $l3 => ${List.tail(l3)}
+         """.stripMargin)
+    }
   }
 }
